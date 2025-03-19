6290a043f3b838bd2b6b5cdf3b094034ba4b66e9
ralf <chrystianralfc@gmail.com>
2025-03-19 12:19:07 -0400

    funciones de agregar productos

--- Practica 2/controllers/productos.controller.js
+++ Practica 2/controllers/productos.controller.js
@@ -6,7 +6,7 @@ const getProductos = async (req, res) => {
 
     try {
         if (mode === 'basic' || mode === 'pool') {
-        db.query('SELECT * FROM productos', (err, results) => {
+        db.query('SELECT p.nombre, p.precio, p.stock, c.nombre AS categoria FROM productos p JOIN LEFT categorias c ON p.id_categoria = c.id_categoria', (err, results) => {
             if (err) return res.status(500).json({ error: err.message });
             res.json({ modo: mode, results });
         });
@@ -19,4 +19,30 @@ const getProductos = async (req, res) => {
     }
 };
 
-module.exports = { getProductos };
+const getProductoById = async (req, res) => {}
+
+const createProducto = async (req, res) => {
+    const mode = req.headers['x-db-mode'] || 'promise';
+    const db = getDBConnection(mode);
+    const { nombre, descripcion, precio, stock, categorias } = req.body;
+
+    try {
+        if (mode === 'basic' || mode === 'pool') {
+            db.query('INSERT INTO productos(`nombre`, `descripcion`, `precio`, `stock`, `id_categoria`) VALUES (?, ?, ?, ?, ?)', [nombre, descripcion, precio, stock, categorias], (err, results) => {
+                if (err) return res.status(500).json({ error: err.message });
+                res.json({ modo: mode, id: results.insertId });
+            });
+        } else {
+            const [rows] = await db.query('INSERT INTO productos(`nombre`, `descripcion`, `precio`, `stock`, `id_categoria`) VALUES (?, ?, ?, ?)', [nombre, descripcion, precio, stock, categorias]);
+            res.json({ modo: mode, id: rows.insertId });
+        }
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+}
+
+const deleteProducto = async(req, res) => {
+
+}
+
+module.exports = { getProductos, getProductoById, createProducto, deleteProducto };
--- Practica 2/routes/productos.route.js
+++ Practica 2/routes/productos.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getProductos } = require('../controllers/productos.controller');
+const { getProductos, getProductoById, createProducto } = require('../controllers/productos.controller');
 
+router.get('/productos/:id', getProductoById);
 router.get('/productos', getProductos);
+router.post('/productos', createProducto);
 
 module.exports = router;
